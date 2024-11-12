0dde08d0c63cc7fc9d6e45cdb2fe460073e35699
TheodoreUtomo <theodoreutomo@gmail.com>
2024-11-12 00:46:19 -0500

    Initial Commit

---
+++ PantryPal/PantryPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  PantryPalApp.swift
+//  PantryPal
+//
+//  Created by Theodore Utomo on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct PantryPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
